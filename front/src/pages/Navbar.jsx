f4ef1214c16d717261edab60129363ed33943180
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-01-04 15:31:00 +0100

    NavBar sans le css

--- front/src/pages/Navbar.jsx
+++ front/src/pages/Navbar.jsx
@@ -1,9 +1,28 @@
+import React from 'react';
+import { useLocation } from "react-router"
+
 export const NavBar=()=>{
+    const location = useLocation();
+    const path = location.pathname;
+
+    // Fonction pour obtenir le texte en fonction de l'URL
+    const getNavbarText = () => {
+        if (path.endsWith('/accueilAdmin')) {
+            return 'Admin';
+        } else if (path.endsWith('/accueilUser')) {
+            return 'Entreprise***';
+        }
+        // Si l'URL ne se termine par aucune des valeurs attendues, retournez une chaîne vide ou un texte par défaut
+        return '';
+    };
 
     return(
         <nav>
             <img src="./imagefront/Nouveau-projet-1.png" alt="logo projet"/>
-            <button><a href="/">Déconnexion</a></button>
+            <div>
+                <span>{getNavbarText()}</span>
+                <button><a href="/">Déconnexion</a></button>
+            </div>  
         </nav>
     )
 }
