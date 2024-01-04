1cf17d13db925f5c224ca358f62fe2ecf1d47926
pieguezou <pieguezou@gmail.com>
2024-01-04 14:46:22 +0100

    commit

--- front/src/pages/connexion.jsx
+++ front/src/pages/connexion.jsx
@@ -1,5 +1,9 @@
 export const Connexion=()=>{
-    
-}
 
-<img className="logo">https://cours.institutsolacroup.com/pluginfile.php/4087/mod_folder/content/0/LogoByMathysG.JPG</img>
+  return(
+      <>
+          <img src="https://cours.institutsolacroup.com/pluginfile.php/4087/mod_folder/content/0/LogoByMathysG.JPG" className="logo"></img>
+          
+      </>
+  )
+};
