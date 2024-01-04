94fcd315118f47b0215b2e2ee8784f43580edf62
pieguezou <pieguezou@gmail.com>
2024-01-04 15:47:24 +0100

    commit

--- front/src/pages/connexion.jsx
+++ front/src/pages/connexion.jsx
@@ -11,7 +11,7 @@ export const Connexion = () => {
         <label htmlFor="entreprise">Entreprise :</label>
         <textarea id="entreprise" name="entreprise" rows="4" cols="50"></textarea>
         <img
-          src="./imagefront/fleche-bas.png"
+          src="./imagefront/front/imagefront/pngtree-vector-down-arrow-icon-png-image_4184901 1.png"
           alt="flèche vers le bas"
           style={{ marginLeft: '10px', cursor: 'pointer' }}
         />
