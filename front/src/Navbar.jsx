e4cfdd6f38d8dfded14fa61962a6f0458519a1d7
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-01-04 09:48:43 +0100

    react

--- front/src/Navbar.jsx
+++ front/src/Navbar.jsx
@@ -3,7 +3,7 @@ export const NavBar=()=>{
     return(
         <nav>
             <img src="./imagefront/Nouveau-projet-1.png" alt="logo projet"/>
-            <button><a ref="/">Déconnexion</a></button>
+            <button><a href="/">Déconnexion</a></button>
         </nav>
     )
 }
