60c1890c1890ff22e2b89bac4e9bd21221397ce6
pieguezou <pieguezou@gmail.com>
2024-01-04 15:42:46 +0100

    commit

--- front/src/pages/connexion.jsx
+++ front/src/pages/connexion.jsx
@@ -7,9 +7,14 @@ export const Connexion = () => {
         <img src="./imagefront/Nouveau-projet-1.png" alt="logo projet" />
       </nav>
 
-      <div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
         <label htmlFor="entreprise">Entreprise :</label>
-        <textarea id="entreprise" name="entreprise" rows="4" cols="30"></textarea>
+        <textarea id="entreprise" name="entreprise" rows="4" cols="50"></textarea>
+        <img
+          src="./imagefront/fleche-bas.png"
+          alt="flèche vers le bas"
+          style={{ marginLeft: '10px', cursor: 'pointer' }}
+        />
       </div>
     </div>
   );
