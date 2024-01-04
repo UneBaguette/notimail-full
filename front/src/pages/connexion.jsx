4552cc7f74c4cdd6829089e62a4e0f43d6930aae
pieguezou <pieguezou@gmail.com>
2024-01-04 15:01:59 +0100

    commit

--- front/src/pages/connexion.jsx
+++ front/src/pages/connexion.jsx
@@ -3,7 +3,6 @@ export const Connexion=()=>{
   return(
     <nav>
         <img src="./imagefront/Nouveau-projet-1.png" alt="logo projet"/>
-        <button><a href="/">Déconnexion</a></button>
     </nav>
 )
 };
