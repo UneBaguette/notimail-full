06bb5e40204c679f8213805b3fb4ea16212740c8
pieguezou <pieguezou@gmail.com>
2024-01-08 11:19:12 +0100

    page connexion

--- front/src/pages/connexion.jsx
+++ front/src/pages/connexion.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 // Définit un composant fonctionnel Connexion
 export const Connexion = () => {
@@ -14,14 +14,14 @@ export const Connexion = () => {
   // Effectue une requête GET pour récupérer la liste des entreprises depuis le backend
   useEffect(() => {
     fetch(`http://localhost:3000/user/users`)
-      .then(result => result.json())
-      .then(data => {
+      .then((result) => result.json())
+      .then((data) => {
         console.log(data);
         setEntreprise(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-      })
+      });
   }, []);
 
   // Fonction appelée lorsque l'utilisateur choisit une entreprise dans le formulaire
@@ -51,15 +51,15 @@ export const Connexion = () => {
         // Authentification réussie
         console.log('Authentification réussie');
         // Redirige l'utilisateur vers la page d'accueil des utilisateurs
-        navigate("/accueilUsers");
+        navigate('/accueilUsers');
       } else {
         // Authentification échouée
         console.error('Authentification échouée');
         setError('Identifiants incorrects');
       }
     } catch (error) {
-      console.error('Erreur lors de l\'authentification:', error);
-      setError('Erreur lors de l\'authentification');
+      console.error("Erreur lors de l'authentification:", error);
+      setError("Erreur lors de l'authentification");
     }
   };
 
@@ -67,32 +67,30 @@ export const Connexion = () => {
   return (
     <div>
       <h2>Connexion</h2>
-      <img src= "/imagefront/Nouveauprojet1.png" alt="Description de l'image"></img>
+      <img src="/imagefront/Nouveauprojet1.png" alt="Description de l'image" />
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           Entreprise:
           <select name="entreprise" value={selectedEntreprise} onChange={handleEntrepriseSelection}>
-            <option value="">
-              Sélectionnez une entreprise
-            </option>
+            <option value="">Sélectionnez une entreprise</option>
             {entreprise.map((entreprise) => (
               <option key={entreprise.id} value={entreprise.firm_name}>
                 {entreprise.firm_name}
               </option>
             ))}
           </select>
-          <img className= "cadenas" src="/imagefront/pngtree-vector-down-arrow-icon-png-image_4184901 1.png" alt="description de l'image"></img>
+          <img
+            className="image-container"
+            src="/imagefront/pngtree-vector-down-arrow-icon-png-image_41849011.png"
+            alt="description de l'image"
+          />
         </label>
         <br />
         <label>
           Mot de Passe:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <img src="/imagefront/pngtree-black-padlock-png-image_3729324 (1) 1.png" alt="description de l'image"></img>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <img src="/imagefront/pngtree-black-padlock-png-image_3729324 (1) 1.png" alt="description de l'image" />
         </label>
         <br />
         <button type="submit">Se connecter</button>
