9f870805a4e720d0eec1d44e4933c0984a7f1b8c
pieguezou <pieguezou@gmail.com>
2024-01-09 15:31:58 +0100

    page acceuilUser

--- front/src/pages/accueilUser.jsx
+++ front/src/pages/accueilUser.jsx
@@ -1,18 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './acceilUser.css';
-
+import { useParams } from "react-router-dom"
 export const AccueilUser = () => {
-
+  
+  const { userId } = useParams()
+console.log(userId)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const response = await fetch(`http://localhost:3000/mail/picked-up-mail/${userId}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+            }),
+          });
+    
+          if (response.ok) {
+    
+          } else {
+            console.error('Récupération échouée');
+          }
+        } catch (error) {
+          console.error("Erreur lors de la réception:", error);
+          setError("Erreur lors de la reception");
+        }
+      };
     return (
         <>
-            <div class='conteneur'>
+            <div className='conteneur'>
                 {/* Image enveloppe */}
                 <img src="/imagefront/44849e8b90ebf9de43ed123e14a739b0.png" alt="enveloppe" />
-                <span class='petit-cercle' />
+                <span className='petit-cercle' />
+                <div className='texte'>
+                <h3>Vous avez du courrier en attente</h3>
+                </div>
+
+                <div className='textes'>
+                  <button onSubmit={handleSubmit}>Réceptionner</button>  
+                </div>
             </div>
-
-
         </>
     )
 }
