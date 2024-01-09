fc9d1b75d89429d40b31fc6895165114d0f15339
Marvin-Morin <marvin.malouin@gmail.com>
2024-01-09 10:43:11 +0100

    permsission 6

--- back/routes/auth.ts
+++ back/routes/auth.ts
@@ -3,13 +3,12 @@
 // Importation du module Express
 import express from 'express';
 
-// // Importation du contrôleur d'authentification
-// import { authUser } from '../controllers/auth';
+// Importation du contrôleur d'authentification
+import { authUser } from '../controllers/auth';
 
-// import { deconnexionUser } from '../controllers/auth';
-
-import { createUser, getUsers, getUserById, updateUser, deleteUser } from '../controllers/users';
-import { isAdmin, isAuthenticated } from '../middlewares/permissions'; // Ajouter ces imports
+import { deconnexionUser } from '../controllers/auth';
+import { isAdmin } from '../middlewares/permissions';
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controllers/users';
 
 
 // Création d'un objet Router d'Express
@@ -18,19 +17,24 @@ const router = express.Router();
 // Toutes les routes auth commence par : auth/connexion
 
 // Route POST pour créer un utilisateur
-router.post('/users', isAuthenticated, isAdmin, createUser);
+router.post('/users',  authUser, isAdmin, createUser)
 
-// Route GET pour récupérer tous les utilisateurs
-router.get('/users', isAuthenticated, isAdmin, getUsers);
+// Définition de la route POST pour récupérer tous les utilisateurs
+router.get('/users', authUser, isAdmin, getUsers);
 
 // Route GET par ID pour récupérer un utilisateur spécifique
-router.get('/users/:id', isAuthenticated, getUserById);
+router.get('/users/:id', authUser, getUserById,)
 
 // Route PUT pour mettre à jour un utilisateur par ID
-router.put('/users/:id', isAuthenticated, isAdmin, updateUser);
+router.put('users/:id', authUser, isAdmin, updateUser)
 
 // Route DELETE pour supprimer un utilisateur par ID
-router.delete('/users/:id', isAuthenticated, isAdmin, deleteUser);
+router.delete('/users/:id', authUser, isAdmin, deleteUser)
+
+// Définition de la route GET '/deconnexion' avec le contrôleur deconnexionUser
+router.get('/deconnexion', deconnexionUser);
+
+
 
 
 // Exportation du routeur pour qu'il puisse être utilisé ailleurs dans l'application
