372db3aa9b6c6f89a811c156d1d056903f23a1ec
pieguezou <pieguezou@gmail.com>
2024-01-09 09:15:21 +0100

    page acceuilUser

--- front/src/pages/accueilUser.jsx
+++ front/src/pages/accueilUser.jsx
@@ -1,10 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { NavBar } from "./Navbar"
+import './acceilUser.css';
 
 export const AccueilUser=()=>{
     
     return(
         <>
             <NavBar />
+
+            <img className='cercle' src="/imagefront/44849e8b90ebf9de43ed123e14a739b0.png" alt="description de l'image" />
         </>
     )
 }
