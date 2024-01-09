4a8a9b2f2cb516cc65b1465bce3fb679f9ced2f6
Marvin-Morin <marvin.malouin@gmail.com>
2024-01-09 11:33:39 +0100

    modi front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AccueilAdmin } from './pages/accueilAdmin'
-import { Mail } from './pages/mail'
-import { AccueilUser } from './pages/accueilUser'
-import { Connexion } from './pages/connexion'
-import { Confirm } from './pages/confirm'
-import { User } from './pages/user'
-import { Edit } from './pages/edit'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AccueilAdmin } from "./pages/accueilAdmin";
+import { Mail } from "./pages/mail";
+import { AccueilUser } from "./pages/accueilUser";
+import { Connexion } from "./pages/connexion";
+import { Confirm } from "./pages/confirm";
+import { User } from "./pages/user";
+import { Edit } from "./pages/edit";
+import { NavBar } from "./pages/Navbar";
 
 const App = () => {
-
   useEffect(() => {
     document.title = "Notimail";
   }, []);
@@ -18,17 +18,18 @@ const App = () => {
 
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Connexion/>}/>
-        <Route path='/accueilUser' element={<AccueilUser/>}/>
-        <Route path='/accueilAdmin' element={<AccueilAdmin/>}/>
-        <Route path='/confirm' element={<Confirm/>}/>
-        <Route path='/mail' element={<Mail/>}/>
-        <Route path='/user' element={<User/>}/>
-        <Route path='/edit' element={<Edit/>}/>
-      </Routes>        
+        <Route path="/" element={<Connexion />} />
+        <Route path="/accueilUser" element={<AccueilUser />} />
+        <Route path="/accueilAdmin" element={<AccueilAdmin />} />
+        <Route path="/confirm" element={<Confirm />} />
+        <Route path="/mail" element={<Mail />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
-export default App;  // Exportez le composant par défaut
+export default App; // Exportez le composant par défaut
--- front/src/pages/accueilUser.jsx
+++ front/src/pages/accueilUser.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { NavBar } from "./Navbar"
 import './acceilUser.css';
 
 export const AccueilUser = () => {
 
     return (
         <>
-            <NavBar />
-
             <div class='conteneur'>
-                <img src="/imagefront/44849e8b90ebf9de43ed123e14a739b0.png" alt="description de l'image" />
-                <div class='petit-cercle'></div>
+                {/* Image enveloppe */}
+                <img src="/imagefront/44849e8b90ebf9de43ed123e14a739b0.png" alt="enveloppe" />
+                <span class='petit-cercle' />
             </div>
 
 
