bc056a4e6a54e332de1ad7643fd45b41cdf4945a
Zicbel <alexandre.b@institutsolacroup.com>
2024-01-10 10:25:32 +0100

    debug courrier

--- back/routes/mail.ts
+++ back/routes/mail.ts
@@ -7,7 +7,7 @@ import { isAdmin, isAuthenticated } from '../middlewares/permissions';
 const router = express.Router();
 
 // Route POST pour indiquer la réception d'un courrier par l'admin a récuperer par le user
-router.post('/reception-mail/:userId', receivedMail, isAdmin);
+router.post('/received-mail/:userId', receivedMail, isAdmin);
 
 // Route POST pour indiquer la récupération d'un courrier par l'user
 router.post('/picked-up-mail/:userId', pickUpMail, isAuthenticated);
