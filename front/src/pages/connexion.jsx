6e855cb8802a6a7ad121123070fdb2e6a3fe0cc6
Marvin-Morin <marvin.malouin@gmail.com>
2024-01-10 15:21:37 +0100

    entreprise value

--- front/src/pages/connexion.jsx
+++ front/src/pages/connexion.jsx
@@ -82,7 +82,7 @@ export const Connexion = () => {
             onChange={handleEntrepriseSelection}
           >
             <option key="" value="">
-              Entreprise
+              Seleconniez votre Entreprise
             </option>
             {entreprise.map((entreprise) => (
               <option key={entreprise} value={entreprise}>
