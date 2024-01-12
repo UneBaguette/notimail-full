64e7f93e60bf8378eb144b64d0387cb6fafafb90
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-01-12 14:59:05 +0100

    affiche detail user

--- front/src/pages/accueilAdmin.jsx
+++ front/src/pages/accueilAdmin.jsx
@@ -20,45 +20,45 @@ export const AccueilAdmin=()=>{
     }, [])
 
     const handleUserClick = (user) => {
-        if (selectedUser === user.id) {
-            setSelectedUser(null);
-        } else {
-            setSelectedUser(user.id);
-        }
+        setSelectedUser((prevSelectedUser) => {
+            return prevSelectedUser === user.id ? null : user.id;
+        });
     };
 
     return(
         <div className="content-container">
             <h2>Liste des Utilisateurs</h2>
-            <div className="contact">
+            <section className="contact">
                 {users.map((user) => (
                     <div key={user.id}>
-                        <div className="align_items">
-                            <h3>{user.firm_name}</h3>
-                            <div 
-                                className={`slideOne ${selectedUser === user.id ? 'slideOneChecked' : ''} ${selectedUser ? 'selected' : ''}`} 
-                                onClick={() => handleUserClick(user)}
-                            >
-                                <input type="checkbox" value="None" id={`slideOne_${user.id}`} name="check" checked />
-                                <label htmlFor={`slideOne_${user.id}`}></label>
+                        <section className="nohide">
+                            <div className="align_items">
+                                <h3>{user.firm_name}</h3>
+                                <div 
+                                    className={`slideOne ${selectedUser === user.id ? 'slideOneChecked' : ''} ${selectedUser ? 'selected' : ''}`} 
+                                    onClick={() => handleUserClick(user)}
+                                >
+                                    <input type="checkbox" value="None" id={`slideOne_${user.id}`} name="check" checked />
+                                    <label htmlFor={`slideOne_${user.id}`}></label>
+                                </div>
                             </div>
-                        </div>
-                        <div className="align_items">
-                            <div className="colomun_items">
-                                <p>Nom Contact</p>
-                                {/* Formatage de la date */}
-                                <p>{new Date(user.last_received_mail).toLocaleDateString()}</p>
-                            </div>    
-                            <img src="../../imagefront/888_edit.png" alt="edit"/>
-                        </div>
-                        <div className={`hidedetail ${selectedUser === user.id ? 'show' : ''}`}>  
+                            <div className="align_items">
+                                <div className="colomun_items">
+                                    <p>Nom Contact</p>
+                                    {/* Formatage de la date */}
+                                    <p>{new Date(user.last_received_mail).toLocaleDateString()}</p>
+                                </div>    
+                                <img src="../../imagefront/888_edit.png" alt="edit"/>
+                            </div>
+                        </section>
+                        <section className={`hidedetail ${selectedUser ? 'show' : ''}`}>  
                             <p>Email: {user.email}</p>
                             <p>Téléphone: {user.phone_number}</p>
                             <p>Indentifiant: {user.id}</p>
-                        </div>
+                        </section>
                     </div>
                 ))}
-            </div>
+            </section>
         </div>
     )
 }
