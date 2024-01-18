dd02f75fcbd7ed5fed8860bbd52767351001a7c9
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-01-18 12:24:14 +0100

    modal

--- front/src/pages/accueilAdmin.jsx
+++ front/src/pages/accueilAdmin.jsx
@@ -3,10 +3,12 @@ import './accueilAdmin.css';
 import { IoAddCircleOutline } from "react-icons/io5";
 import { BiMailSend } from "react-icons/bi";
 import { SearchUser } from './searchUser';
+import Modal from 'react-modal';
 
 export const AccueilAdmin=()=>{
     const [users, setUsers] = useState([]);
-    const [selectedUser, setSelectedUser] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedUsers, setSelectedUsers] = useState([]);
     const [selectedUserSelect, setSelectedUserSelect] = useState(null);
 
     // Effectue une requête GET pour récupérer la liste des catégories
@@ -21,7 +23,7 @@ export const AccueilAdmin=()=>{
             .catch(Error => {
                 console.log(Error);
             })
-    }, [])    
+    }, [])
     
     const handleUserClick = (user, event) => {
         console.log('handleUserClick called');
@@ -29,7 +31,7 @@ export const AccueilAdmin=()=>{
     
         if (event.target.classList.contains('nohide')) {
             console.log('Clicked on .nohide element');
-            setSelectedUser((prevSelectedUser) => {
+            setSelectedUsers((prevSelectedUser) => {
                 return prevSelectedUser === user.id ? null : user.id;
             });
         }
@@ -49,55 +51,72 @@ export const AccueilAdmin=()=>{
     };
 
     return(
-        <div className="content-container">
-            {/* <h2>Liste des Utilisateurs</h2> */}
-            <SearchUser />
-            <section className="contact">
-                {users.map((user) => (
-                    <div key={user.id}>
-                        <section className="top">
-                            <div className="align_items">
-                                <h3
-                                    className={`nohide ${selectedUser === user.id ? 'show' : ''}`}
-                                    onClick={(event) => handleUserClick(user, event)}
-                                >
-                                    {user.firm_name}
-                                </h3>
-                                <div 
-                                    className={`slideOne ${selectedUserSelect === user.id ? 'slideOneChecked' : ''} ${selectedUserSelect ? 'selected' : ''}`} 
-                                    onClick={() => handleUserClickSelect(user)}
-                                >
-                                    <input type="checkbox" value="None" id={`slideOne_${user.id}`} name="check" checked />
-                                    <label htmlFor={`slideOne_${user.id}`}></label>
+        <>
+            <div className="content-container">
+                {/* <h2>Liste des Utilisateurs</h2> */}
+                <SearchUser />
+                <section className="contact">
+                    {users.map((user) => (
+                        <div key={user.id}>
+                            <section className="top">
+                                <div className="align_items">
+                                    <h3
+                                        className={`nohide ${selectedUsers === user.id ? 'show' : ''}`}
+                                        onClick={(event) => handleUserClick(user, event)}
+                                    >
+                                        {user.firm_name}
+                                    </h3>
+                                    <div 
+                                        className={`slideOne ${selectedUserSelect === user.id ? 'slideOneChecked' : ''} ${selectedUserSelect ? 'selected' : ''}`} 
+                                        onClick={() => handleUserClickSelect(user)}
+                                    >
+                                        <input type="checkbox" value="None" id={`slideOne_${user.id}`} name="check" checked />
+                                        <label htmlFor={`slideOne_${user.id}`}></label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="align_items">
-                                <div className="colomun_items">
-                                    <p>Nom Contact</p>
-                                    {/* Formatage de la date */}
-                                    <p>{new Date(user.last_received_mail).toLocaleDateString()}</p>
+                                <div className="align_items">
+                                    <div className="colomun_items">
+                                        <p>Nom Contact</p>
+                                        {/* Formatage de la date */}
+                                        <p>{new Date(user.last_received_mail).toLocaleDateString()}</p>
+                                    </div>
+                                    <a href="/ajoutEntreprise">   
+                                        <img src="../../imagefront/888_edit.png" alt="edit"/>
+                                    </a> 
                                 </div>
-                                <a href="/ajoutEntreprise">   
-                                    <img src="../../imagefront/888_edit.png" alt="edit"/>
-                                </a> 
-                            </div>
-                        </section>
-                        <section className={`hidedetail ${selectedUser === user.id ? 'show' : ''}`}>
-                            <p>Email: {user.email}</p>
-                            <p>Téléphone: {user.phone_number}</p>
-                            <p>Indentifiant: {user.id}</p>
-                        </section>
+                            </section>
+                            <section className={`hidedetail ${selectedUsers === user.id ? 'show' : ''}`}>
+                                <p>Email: {user.email}</p>
+                                <p>Téléphone: {user.phone_number}</p>
+                                <p>Indentifiant: {user.id}</p>
+                            </section>
+                        </div>
+                    ))}
+                </section>
+                <section class="ajoutUser">
+                    <a href="/ajoutEntreprise" className="blue-background">
+                        <IoAddCircleOutline />
+                    </a>
+                    <a href="#" className="blue-background">
+                        <BiMailSend />
+                    </a>
+                </section>
+            </div>
+
+            {/* Modale pour les utilisateur selectionner */}
+            <Modal isOpen={showModal}>
+                <div className='modal-overlay'>
+                    <div className='modal'>
+                    <h3>Liste des utilisateurs sélectionnés</h3>
+                    {/* Affichez ici la liste des utilisateurs sélectionnés */}
+                    {selectedUsers.map((user) => (
+                        <div key={user.id}>{user.firm_name}</div>
+                    ))}
+                    <button onClick={() => setShowModal(false)}>Fermer</button>
                     </div>
-                ))}
-            </section>
-            <section class="ajoutUser">
-                <a href="/ajoutEntreprise" className="blue-background">
-                    <IoAddCircleOutline />
-                </a>
-                <a href="#" className="blue-background">
-                    <BiMailSend />
-                </a>
-            </section>
-        </div>
+                </div>
+            </Modal>
+
+        </>
     )
 }
