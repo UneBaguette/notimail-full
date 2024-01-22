9af2492dacdf1bd08d0f2e9c3e3f7449b963b67c
UneBaguette <28904802+UneBaguette@users.noreply.github.com>
2024-01-22 09:58:18 +0100

    fixed tests admin token

--- back/__tests__/api.spec.ts
+++ back/__tests__/api.spec.ts
@@ -3,6 +3,7 @@ import Test from "supertest/lib/test";
 import app from "../src/app";
 import supertest from "supertest";
 import { User } from "../src/models/users";
+import bcrypt from "bcrypt";
 
 export const close = async (users: User[]): Promise<void> => {
   try {
@@ -19,6 +20,28 @@ export const close = async (users: User[]): Promise<void> => {
 
 export const getAdminTokenTest = async (): Promise<string> => {
     try {
+
+      const admin = await User.findOne({ where: { firm_name: "admintest" } });
+
+      if (admin && !admin.is_admin) {
+        await User.delete({firm_name: "admintest"});
+      }
+
+      if (!admin) {
+        const admin = User.create([{
+          firm_name: "admintest",
+          first_name: "admin",
+          last_name: "test",
+          email: "test@test.com",
+          password: await bcrypt.hash("admin", 10),
+          phone_number: "098765453",
+          last_picked_up: new Date(),
+          last_received_mail: new Date(),
+          is_admin: true
+        }]);
+        await User.insert(admin);
+      }
+
       const response = await supertest(app)
         .post('/auth/connexion')
         .send({
