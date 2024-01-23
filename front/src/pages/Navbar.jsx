c09c4b82e6433517e50470ab65c86923122cc270
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-01-23 12:01:01 +0100

    navbar

--- front/src/pages/Navbar.jsx
+++ front/src/pages/Navbar.jsx
@@ -44,7 +44,7 @@ export const NavBar=()=>{
 
     return(
         <>        
-            {(pathname === '/' || pathname === '/ajoutEntreprise' || pathname === '/modiferEntreprise') ? null : (
+            {(pathname === '/' || pathname === '/ajoutEntreprise' || pathname === '/modiferEntreprise/:id') ? null : (
                 <nav>
                     <div id="left-content">
                         <img src="/imagefront/Nouveauprojet1.png" alt="logo projet"/>
